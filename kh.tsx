4d734e84a69365eda754df371a7834db54cac184
T-I-M-M-O-I-N <78977650+T-I-M-M-O-I-N@users.noreply.github.com>
2021-02-16 17:27:25 +0100

    Delete kh.tsx

--- kh.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="kh" tilewidth="32" tileheight="32" tilecount="2220" columns="60">
- <image source="../../../../Downloads/star wars pictures/Epic Star Wars Wallpaper (65+ images)_files/316395.jpg" width="1920" height="1200"/>
-</tileset>
