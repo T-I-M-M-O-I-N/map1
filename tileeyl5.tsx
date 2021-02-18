445b076fe8ff40eaa7daabff7597b71cb052562e
T-I-M-M-O-I-N <78977650+T-I-M-M-O-I-N@users.noreply.github.com>
2021-02-18 08:34:39 +0000

    Deploying to gh-pages from master @ 78ce0942879f0256364f8d1d9a33383be916c143 🚀

---
+++ tileeyl5.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileeyl5" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../Bilder/tileeyl5.png" width="512" height="512"/>
+ <tile id="128">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
